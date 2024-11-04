dc658afca0fc4be711d3a67045156621aebd0709
Laura Monne <laura93monne@uoc.edu>
2024-11-04 15:57:03 +0100

    assays i coldata

--- Monne-Laura-PEC1.R
+++ Monne-Laura-PEC1.R
@@ -12,3 +12,11 @@ colnames(GastricCancer_NMR)#visualitzem el nom de les columnes
 head(GastricCancer_NMR) # mostrem les primeres files
 summary(GastricCancer_NMR[, c("M1", "M2", "M3", "M4", "M5")]) #generem estadistiques resum de les primeres columnes
 str(GastricCancer_NMR) #classe de dades en cada columna
+#Dividim les dades en assays i coldata
+assays <- t(as.matrix(GastricCancer_NMR[, 5:ncol(GastricCancer_NMR)]))#creem la matriu
+#transposada amb les columnes amb dades quantitatives; cada columna es una mostra
+dim(assays) #comprovem les dimensions de la matriu
+colnames(assays) <- GastricCancer_NMR$SampleID #assignem un nom (sampleID) a cada columna (monstra)
+#creem un dataframe amb dades SampleID, SampleType i Class sobre les mostres
+colData <- DataFrame(SampleID = GastricCancer_NMR$SampleID, SampleType = GastricCancer_NMR$SampleType, Class = GastricCancer_NMR$Class)
+dim(colData) #comprovem les dimensions
